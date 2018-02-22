1a5164c22037052ee4892c5841019e50eee49281
Transnavigators <Transnavigators>
2018-02-21 18:20:22 -0800

    Fixed more encoding errors and variables

--- i2ctest.py
+++ i2ctest.py
@@ -1,14 +1,13 @@
 import smbus
 import time
-# set up busù http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2
 bus = smbus.SMBus(1)
 
 # This is the address we setup in the Arduino Program
 address = 0x04
-move_cmd = 'm'
+move_cmd = ord('m')
 
 def writeNumber(value1,value2):
-    bus.write_i2c_block_data(address, move_cmd, value1, value2, 0);
+    bus.write_i2c_block_data(address, move_cmd, [value1, value2, 0]);
     # bus.write_byte_data(address, 0, value)
     return -1
 
@@ -22,7 +21,7 @@ while True:
     s2 = input("Enter 1 - 256: ")
 
     writeNumber(s1,s2)
-    print "RPI: Hi Arduino, I sent you ", var
+    print "RPI: Hi Arduino, I sent you ", s1, s2 
     # sleep one second
     time.sleep(1)
 

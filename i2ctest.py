c20d77641c4659c0c6fb423a723f01da9e934c50
U-CAMPUS\yuenm18 <yuenm18@EN306-18526.campus.up.edu>
2018-02-21 18:05:06 -0800

    Fixed encoding

--- i2ctest.py
+++ i2ctest.py
@@ -1,6 +1,6 @@
 import smbus
 import time
-# for RPI version 1, use “bus = smbus.SMBus(0)” http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2
+# set up bus� http://www.raspberry-projects.com/pi/programming-in-python/i2c-programming-in-python/using-the-i2c-interface-2
 bus = smbus.SMBus(1)
 
 # This is the address we setup in the Arduino Program
@@ -8,23 +8,23 @@ address = 0x04
 move_cmd = 'm'
 
 def writeNumber(value1,value2):
-	bus.write_i2c_block_data(address, move_cmd, value1, value2, 0);
-	# bus.write_byte_data(address, 0, value)
-	return -1
+    bus.write_i2c_block_data(address, move_cmd, value1, value2, 0);
+    # bus.write_byte_data(address, 0, value)
+    return -1
 
 def readNumber():
-	number = bus.read_byte(address)
-	# number = bus.read_byte_data(address, 1)
-	return number
+    number = bus.read_byte(address)
+    # number = bus.read_byte_data(address, 1)
+    return number
 
 while True:
-	s1 = input("Enter 1 – 256: ")
-	s2 = input("Enter 1 – 256: ")
+    s1 = input("Enter 1 - 256: ")
+    s2 = input("Enter 1 - 256: ")
 
-	writeNumber(s1,s2)
-	print "RPI: Hi Arduino, I sent you ", var
-	# sleep one second
-	time.sleep(1)
+    writeNumber(s1,s2)
+    print "RPI: Hi Arduino, I sent you ", var
+    # sleep one second
+    time.sleep(1)
 
-	number = readNumber()
-	print "Arduino: Hey RPI, I received a digit ", number
+    number = readNumber()
+    print "Arduino: Hey RPI, I received a digit ", number

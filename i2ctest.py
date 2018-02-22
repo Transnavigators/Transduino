acbe8d8c66e5cc0cd1a71b27f1b9a6ce66e597e3
U-CAMPUS\yuenm18 <yuenm18@EN306-18526.campus.up.edu>
2018-02-21 18:30:23 -0800

    Tried to fix number format error

--- i2ctest.py
+++ i2ctest.py
@@ -11,7 +11,7 @@ move_cmd = ord('m')
 encoder_cmd = ord('e')
 
 def writeNumber(m1,m2):
-    bus.write_i2c_block_data(address, move_cmd, [value1, value2, 0]);
+    bus.write_i2c_block_data(address, move_cmd, [m1, m2, 0]);
     
     return -1
 
@@ -20,10 +20,19 @@ def readNumber():
     # number = bus.read_byte_data(address, 1)
     return number
 
+def convertToByte(byte):
+	if byte > 127:
+		return (256-byte) * (-1)
+	else:
+		return byte
+	
 while True:
     m1 = input("Enter value for motor 1 [-127,127] : ")
     m2 = input("Enter value for motor 2 [-127,127] : ")
 
+	m1 = convertToByte(m1)
+	m2 = convertToByte(m2)
+	
     writeNumber(m1,m2)
 	
     print "Motor 1: ", m1, "Motor 2: ", m2

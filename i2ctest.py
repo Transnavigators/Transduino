3d3b02dd3e72350b26f976ec84f78da30bcab787
U-CAMPUS\yuenm18 <yuenm18@EN306-18526.campus.up.edu>
2018-02-21 18:26:16 -0800

    Fixed up messages

--- i2ctest.py
+++ i2ctest.py
@@ -1,14 +1,18 @@
 import smbus
 import time
+
+#initialize the bus
 bus = smbus.SMBus(1)
 
-# This is the address we setup in the Arduino Program
+# Arduino i2c address
 address = 0x04
-move_cmd = ord('m')
 
-def writeNumber(value1,value2):
+move_cmd = ord('m')
+encoder_cmd = ord('e')
+
+def writeNumber(m1,m2):
     bus.write_i2c_block_data(address, move_cmd, [value1, value2, 0]);
-    # bus.write_byte_data(address, 0, value)
+    
     return -1
 
 def readNumber():
@@ -17,11 +21,13 @@ def readNumber():
     return number
 
 while True:
-    s1 = input("Enter 1 - 256: ")
-    s2 = input("Enter 1 - 256: ")
+    m1 = input("Enter value for motor 1 [-127,127] : ")
+    m2 = input("Enter value for motor 2 [-127,127] : ")
 
-    writeNumber(s1,s2)
-    print "RPI: Hi Arduino, I sent you ", s1, s2 
+    writeNumber(m1,m2)
+	
+    print "Motor 1: ", m1, "Motor 2: ", m2
+	
     # sleep one second
     time.sleep(1)
 

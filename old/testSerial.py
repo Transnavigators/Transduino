979632a85427fdff196a246c86a24b830a285dfa
Matthew Yuen <yuenm18@up.edu>
2018-04-30 22:54:35 -0700

    Deleted old docs

--- old/testSerial.py
+++
@@ -1,7 +0,0 @@
-import serial
-
-ser = serial.Serial(port='/dev/ttyACM0',baudrate=115200,timeout=0)
-while True:
-	data = ser.readline()
-	if data and data != "\n" and data != "\n\r" and data != "\r\n":
-		print data
